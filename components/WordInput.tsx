00cc7ea50b960ad5728b2bdc1dd9a55dd4a60cb9
Emre Karayaman <karayaman.emre@outlook.com>
2024-05-12 16:33:20 +0300

    chore: Update WordInput component to focus input on word change

--- components/WordInput.tsx
+++ components/WordInput.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { CornerDownLeft } from "lucide-react";
@@ -15,6 +15,12 @@ const WordInput: React.FC<WordInputProps> = ({ onSubmit, word, setWord }) => {
 	const languageCode = useGetLanguage();
 	const isTurkish = languageCode === "tr";
 
+	useEffect(() => {
+		if (inputRef.current) {
+			inputRef.current.focus();
+		}
+	}, [word]);
+
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		onSubmit(word);
@@ -22,7 +28,6 @@ const WordInput: React.FC<WordInputProps> = ({ onSubmit, word, setWord }) => {
 	};
 
 	const placeholderText = isTurkish ? "KELİME GİRİN" : "ENTER A WORD";
-	const uppercasePlaceholder = placeholderText.toUpperCase();
 
 	return (
 		<form
@@ -37,12 +42,10 @@ const WordInput: React.FC<WordInputProps> = ({ onSubmit, word, setWord }) => {
 						setWord(e.target.value);
 						if (inputRef.current) inputRef.current.focus();
 					}}
-					placeholder={uppercasePlaceholder}
+					placeholder={placeholderText}
 					autoComplete='off'
 					autoFocus
-					className={`h-[50px] text-3xl border-cream text-cream py-2 outline-none focus:ring-0 border-b-2 shadow-[0px_4px_0px_#000000] rounded-none lowercase ${
-						isTurkish ? "placeholder:uppercase" : ""
-					}`}
+					className={`h-[50px] text-3xl border-cream text-cream py-2 outline-none focus:ring-0 border-b-2 shadow-[0px_4px_0px_#000000] rounded-none lowercase`}
 				/>
 				<Button
 					variant='ghost'
