b099c87bf4de1d42b356cffbe599b81930a98a79
Emre Karayaman <karayaman.emre@outlook.com>
2024-05-12 20:20:03 +0300

    chore: Update dictionary API endpoint to use relative path

--- app/api/dictionary/route.ts
+++ app/api/dictionary/route.ts
@@ -1,10 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-	const data = await fetch(`https://spellbee-rho.vercel.app/dictionary.json`);
-	if (!data.ok) {
-		throw new Error("Failed to load dictionary");
+export async function GET(res: NextApiResponse, req: NextApiRequest) {
+	res.setHeader(
+		"Access-Control-Allow-Origin",
+		"https://spellbee-rho.vercel.app/"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+	if (req.method === "OPTIONS") {
+		res.status(200).end();
+		return;
 	}
-	const dictionary = await data.json();
-	return NextResponse.json(dictionary);
+
+	try {
+		const data = await fetch(`https://spellbee-rho.vercel.app/dictionary.json`);
+		if (!data.ok) {
+			throw new Error("Failed to load dictionary");
+		}
+		const dictionary = await data.json();
+		return NextResponse.json(dictionary);
+	} catch (error) {
+		console.error("Error loading the dictionary:", error);
+	}
 }
