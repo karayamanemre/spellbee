4b8c974ff6f8534895d331ed56fb5a3ebc23dd06
Emre Karayaman <karayaman.emre@outlook.com>
2024-05-20 22:19:28 +0300

    refactor: Update Timer component to display additional time in animation

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -9,6 +9,7 @@ interface TimerProps {
 const Timer: React.FC<TimerProps> = ({ initialTime, onTimeUp, addTime }) => {
 	const [timeLeft, setTimeLeft] = useState(initialTime);
 	const [showAnimation, setShowAnimation] = useState(false);
+	const [animationAddTime, setAnimationAddTime] = useState(0); // Store the addTime for animation
 	const intervalId = useRef<NodeJS.Timeout | null>(null);
 
 	const formatTime = (seconds: number) => {
@@ -45,8 +46,12 @@ const Timer: React.FC<TimerProps> = ({ initialTime, onTimeUp, addTime }) => {
 	useEffect(() => {
 		if (addTime > 0) {
 			setTimeLeft((prevTime) => prevTime + addTime);
+			setAnimationAddTime(addTime); // Update animationAddTime when addTime changes and is greater than 0
 			setShowAnimation(true);
-			setTimeout(() => setShowAnimation(false), 1000);
+			setTimeout(() => {
+				setShowAnimation(false);
+				setAnimationAddTime(0); // Reset animationAddTime after animation
+			}, 1000);
 		}
 	}, [addTime]);
 
@@ -54,8 +59,8 @@ const Timer: React.FC<TimerProps> = ({ initialTime, onTimeUp, addTime }) => {
 		<div className='flex items-center justify-center border-4 bg-cream rounded-md p-1 shadow-[0px_3px_1px] border-primary w-32 relative drop-shadow-2xl'>
 			<p className='font-bold text-xl sm:text-3xl'>{formatTime(timeLeft)}</p>
 			{showAnimation && (
-				<p className='time-add-animation ml-[88px] text-xl font-bold absolute top-2 -right-10'>
-					+{addTime}
+				<p className='time-add-animation text-2xl font-bold'>
+					+{animationAddTime}
 				</p>
 			)}
 		</div>
