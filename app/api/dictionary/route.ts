8dd294ce0cbcddea6311db8c0e424467808d4843
Emre Karayaman <karayaman.emre@outlook.com>
2026-08-25 02:14:50 +0300

    Update app/api/dictionary/route.ts for bilingual engine

--- app/api/dictionary/route.ts
+++ app/api/dictionary/route.ts
@@ -1,10 +1,6 @@
 import { NextResponse } from "next/server";
+import dictionary from "../../../public/dictionary.json";
 
 export async function GET() {
-	const data = await fetch(`https://spellbee-rho.vercel.app/dictionary.json`);
-	if (!data.ok) {
-		throw new Error("Failed to load dictionary");
-	}
-	const dictionary = await data.json();
 	return NextResponse.json(dictionary);
 }
